02f32a38a8f524016a811d5dc2f5c44e4f87f100
Sanny <s.grave@varengold.de>
2024-03-12 11:29:32 +0100

    #30: changed Categories

--- src/main/java/model/Category.java
+++ src/main/java/model/Category.java
@@ -1,26 +1,11 @@
 package model;
 
 public record Category(int categoryId,
-                       String randomCategory,
-                       String generalKnowledge,
-                       String videoGames,
-                       String history,
-                       String scienceNature,
-                       String film,
-                       String mythology,
-                       String books,
-                       String geography,
-                       String scienceMath,
-                       String music,
-                       String comics,
-                       String television,
-                       String animals,
-                       String cartoonAndAnimation,
-                       String politics,
-                       String sports,
-                       String japaneseAnimeAndManga,
-                       String vehicles,
-                       String computers,
-                       String celebrities,
-                       String art) {
+                       String projectManagement,
+                       String network,
+                       String qualityManagement,
+                       String itSecurity,
+                       String itSystems,
+                       String business,
+                       String software) {
 }
