2f218622ecd1c82c008e6e0200b4dbb268fdb1b3
Sanny <s.grave@varengold.de>
2024-03-14 11:27:06 +0100

    #22: Datenbankverbindung

--- src/main/java/de/bs14/lf8/Lf8Application.java
+++ src/main/java/de/bs14/lf8/Lf8Application.java
@@ -1,6 +1,7 @@
 package de.bs14.lf8;
 
 import de.bs14.lf8.repository.CategoryRepository;
+import de.bs14.lf8.repository.PlayerRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -13,6 +14,7 @@ import org.springframework.boot.context.properties.EnableConfigurationProperties
 public class Lf8Application implements CommandLineRunner {
 
 	private final CategoryRepository categoryRepository;
+	private final PlayerRepository playerRepository;
 	public static void main(String[] args) {
 		new SpringApplicationBuilder().sources(Lf8Application.class).run(args);
 	}
@@ -20,6 +22,8 @@ public class Lf8Application implements CommandLineRunner {
 	@Override
 	public void run(String... args) throws Exception {
 		while(true){
+			long outputPlayer = playerRepository.count();
+			System.out.println(outputPlayer);
 			long output= categoryRepository.count();
 			System.out.println(output);
 		}
--- src/main/java/de/bs14/lf8/model/Player.java
+++ src/main/java/de/bs14/lf8/model/Player.java
@@ -1,8 +1,44 @@
 package de.bs14.lf8.model;
 
-public record Player (int playerId,
-                      String name,
-                      int rankingPoints,
-                      String rankingTitle,
-                      String playerPassword){
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Entity
+@Table(name = "Player")
+@Getter
+@ToString
+@NoArgsConstructor
+@Setter
+public class Player {
+
+  @Id
+  @Column(name = "PlayerID")
+  private int playerId;
+
+  @Column(name = "PlayerName")
+  private String playerName;
+
+  @Column(name = "PlayerPassword")
+  private String playerPassword;
+
+  @Column(name = "RankingPoints")
+  private int rankingPoints;
+
+  @Column(name = "RankingTitle")
+  private String rankingTitle;
+
+  public Player(int playerId, String playerName, String playerPassword, int rankingPoints, String rankingTitle) {
+    this.playerId = playerId;
+    this.playerName = playerName;
+    this.playerPassword = playerPassword;
+    this.rankingPoints = rankingPoints;
+    this.rankingTitle = rankingTitle;
+  }
 }
+
