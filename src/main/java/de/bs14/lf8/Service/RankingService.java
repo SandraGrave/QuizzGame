bfaeb3a40db94b89c090e2bcb2fc1ce132f6cfef
espen_mat <espen.matschl@live.de>
2024-04-05 09:13:14 +0200

    rankingTitel

--- src/main/java/de/bs14/lf8/Service/RankingService.java
+++ src/main/java/de/bs14/lf8/Service/RankingService.java
@@ -1,4 +1,4 @@
-package de.bs14.lf8.Service;
+/*package de.bs14.lf8.Service;
 
 import de.bs14.lf8.model.Player;
 import de.bs14.lf8.repository.PlayerRepository;
@@ -27,4 +27,53 @@ public class RankingService {
 
   }
 }
+*/
+package de.bs14.lf8.Service;
+
+import de.bs14.lf8.model.Player;
+import de.bs14.lf8.repository.PlayerRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Sort;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Service
+public class RankingService {
+
+  private final PlayerRepository playerRepository;
+
+  public void showRankingList() {
+    String nextLine = System.lineSeparator();
+    String playerFormat = "%-11s | %-10s | %-4s";
+
+    System.out.printf(playerFormat, "Spielername", "Rangpunkte", "Rangtitel" + nextLine);
+
+    Sort sort = Sort.by("rankingPoints").descending();
+    Iterable<Player> sortPlayerList = playerRepository.findAll(sort);
+
+    for (Player player : sortPlayerList) {
+      System.out.printf(playerFormat, player.getPlayerName(), player.getRankingPoints(), player.getRankingTitle() + nextLine);
+    }
+
+  }
+  public String rankingTitle(int rankingPoints) {
+
+    if (rankingPoints >= 120) {
+      return "Gold";
+    } else if (rankingPoints >= 90) {
+      return "Silver";
+    } else if (rankingPoints >= 50) {
+      return "Bronze";
+    } else if (rankingPoints >= 10) {
+      return "Iron";
+    } else {
+      return "Noch keinen Titel";
+    }
+
+  }
+
+
+
+}
+
 
