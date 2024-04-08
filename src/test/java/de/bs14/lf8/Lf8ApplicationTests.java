de3b8499410d7094d443d178f590458da1789f0c
Levin Bredow <l3vin.bredow@gmail.com>
2024-04-08 14:56:21 +0200

    o

--- src/test/java/de/bs14/lf8/Lf8ApplicationTests.java
+++ src/test/java/de/bs14/lf8/Lf8ApplicationTests.java
@@ -1,13 +1,1 @@
 package de.bs14.lf8;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class Lf8ApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
-}
